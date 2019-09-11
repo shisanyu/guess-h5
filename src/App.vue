949c23fe9f4c43b9d645442245ac560a52a44886
liouyang <liouyang@easylabplus.com>
2019-09-11 10:03:22 +0800

    构架项目

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <keep-alive>
+      <transition name="fade-transform" mode="out-in">
+        <router-view v-if="$route.meta.keepAlive"></router-view>
+      </transition>
+    </keep-alive>
+    <transition name="fade-transform" mode="out-in">
+      <router-view v-if="!$route.meta.keepAlive"></router-view>
+    </transition>
+  </div>
+</template>
+
+<style lang="scss">
+@import './style/index.scss';
+</style>
