905673890401fc03807e680b4b941982ade49935
shisanyu <632094714@qq.com>
2019-09-14 13:57:56 +0800

    银行卡详情

---
+++ src/views/User/BankcardEdit.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BankcardEdit",
+  data() {
+    return {
+
+    };
+  },
+  mounted() {},
+  methods: {
+
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/color.scss";
+a {
+  display: block;
+}
+.container {
+
+}
+</style>
+
+<style lang="css" scoped>
+</style>
